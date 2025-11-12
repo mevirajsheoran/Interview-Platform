13ccf90d560c8a3b12fa0032b2f3312c7b72a309
Viraj Sheoran <viraj.sheoran10@gmail.com>
2025-11-12 19:19:08 +0530

    inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,27 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";    
+import { serve } from "inngest/serve";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+//middleware
+
+app.use(express.json());
+
+//client url is the frontend url
+// credentials:true meaning?? => server allows a browser to include cookies on request
+
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.get("/health", (req, res) => {
     res.send("Server is running");
