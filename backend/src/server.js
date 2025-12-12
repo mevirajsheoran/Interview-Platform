7c2e9ac1765a35ac22edd7b375b168d6f56bfff7
Viraj Sheoran <viraj.sheoran10@gmail.com>
2025-12-13 05:29:36 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+import {clerkMiddleware} from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
+
+
+const app = express();
+
+const __dirname = path.resolve();
+
+//middleware
+
+app.use(express.json());
+
+//client url is the frontend url
+// credentials:true meaning?? => server allows a browser to include cookies on request
+
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); //this add auth field to the request object: req.auth()
+
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
+
+
+app.get("/health", (req, res) => {
+    res.send("Server is running");
+});
+
+
+
+//make our app ready for production
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+    } catch (error) {
+        console.error("💥 Error starting the server", error);
+    }
+};
+
+startServer();
